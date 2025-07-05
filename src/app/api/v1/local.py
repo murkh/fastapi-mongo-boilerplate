b331810e31227d928adb377d2c036be021142fdb
Gaurav Panchal <gkp.125@gmail.com>
2025-07-05 20:50:30 +0530

    Fix parameter name in list_s3 function to use 'path' instead of 'prefix'

--- src/app/api/v1/local.py
+++ src/app/api/v1/local.py
@@ -6,7 +6,7 @@ router = APIRouter(prefix="/local", tags=["LOCAL Storage"])
 
 
 @router.get("/list")
-def list_s3(prefix: str = ""):
+def list_s3(path: str = ""):
     try:
         return {"Hello": "World"}
     except Exception as e:
