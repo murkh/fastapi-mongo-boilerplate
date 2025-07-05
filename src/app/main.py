5bc219d3aa110cf9aebe031e50d24bfa0e824888
Gaurav Panchal <gkp.125@gmail.com>
2025-07-05 22:36:28 +0530

    Add Prometheus configuration and integrate instrumentation for FastAPI

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,4 @@
+from prometheus_fastapi_instrumentator import Instrumentator
 from .core.setup import create_application
 from .core.config import settings
 from .api import router
@@ -6,3 +7,4 @@ load_dotenv()
 
 
 app = create_application(router=router, settings=settings)
+Instrumentator().instrument(app).expose(app)
